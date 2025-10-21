7b336aab8e7aff4b7b11a5075c4144b249733f8a
Gustavo Fontana <gustavoasf77@gmail.com>
2025-10-20 22:27:00 -0300

    serialization

--- src/main/java/br/com/springboot/rest_with_spring_boot_java/data/dto/PersonDTO.java
+++ src/main/java/br/com/springboot/rest_with_spring_boot_java/data/dto/PersonDTO.java
@@ -1,22 +1,40 @@
 package br.com.springboot.rest_with_spring_boot_java.data.dto;
 
-import jakarta.persistence.*;
+import br.com.springboot.rest_with_spring_boot_java.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "firstName", "lastName", "gender", "addressName",})
+@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date birthDay;
+
 
     private String addressName;
 
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phoneNumber;
+
+    private String sensitiveData;
 
     public PersonDTO() {
     }
@@ -61,14 +79,38 @@ public class PersonDTO implements Serializable {
         this.gender = gender;
     }
 
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof PersonDTO person)) return false;
-        return getId() == person.getId() && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddressName(), person.getAddressName()) && Objects.equals(getGender(), person.getGender());
+        if (!(o instanceof PersonDTO personDTO)) return false;
+        return getId() == personDTO.getId() && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddressName(), personDTO.getAddressName()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getPhoneNumber(), personDTO.getPhoneNumber()) && Objects.equals(getSensitiveData(), personDTO.getSensitiveData());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getAddressName(), getGender());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getBirthDay(), getAddressName(), getGender(), getPhoneNumber(), getSensitiveData());
     }
 }
---
+++ src/main/java/br/com/springboot/rest_with_spring_boot_java/serializer/GenderSerializer.java
@@ -0,0 +1,21 @@
+package br.com.springboot.rest_with_spring_boot_java.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class GenderSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(
+            String gender,
+            JsonGenerator jsonGenerator,
+            SerializerProvider serializerProvider
+    ) throws IOException {
+        String formatedGender = "Male".equals(gender) ? "M" : "F";
+
+        jsonGenerator.writeString(formatedGender);
+    }
+}
